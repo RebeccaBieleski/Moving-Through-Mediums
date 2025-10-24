6d993bc3472004eade83494483513da5e3ee3c41
Sylky <miiyusli@gmail.com>
2025-10-25 12:14:19 +1300

    jump, and increase global gravity

--- Moving Through Mediums/Assets/Dev/Sylvie/CharacterController.cs
+++ Moving Through Mediums/Assets/Dev/Sylvie/CharacterController.cs
@@ -3,24 +3,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float MoveSpeed = 50f;
+    [SerializeField] float MoveSpeed = 30f;
+    [SerializeField] float JumpForce = 5f;
     [SerializeField] float HorizontalDragFactor = 0.85f;
 
-    private InputAction _inputAction;
+    private InputAction _moveInput;
+    private InputAction _jumpInput;
     private Rigidbody _rigidBody;
     private Character _character;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _inputAction = InputSystem.actions.FindAction("Move");
+        _moveInput = InputSystem.actions.FindAction("Move");
+        _jumpInput = InputSystem.actions.FindAction("Jump");
         _rigidBody = GetComponent<Rigidbody>();
         _character = GetComponent<Character>();
     }
 
     private void Update()
     {
-        Interact();
+        Jump();
     }
 
     private void FixedUpdate()
@@ -35,21 +38,24 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        var xMovementCommand = _inputAction.ReadValue<Vector2>().x;
+        var xMovementCommand = _moveInput.ReadValue<Vector2>().x;
         _rigidBody.AddForce(new Vector2(xMovementCommand * MoveSpeed, 0));
+    }
+
+    private void Jump()
+    {
+        if (_jumpInput.triggered)
+        {
+            _rigidBody.AddForce(new Vector2(0, JumpForce), ForceMode.Impulse);
+        }
     }
 
     private void UpdateFacing()
     {
-        var xMovementCommand = _inputAction.ReadValue<Vector2>().x;
+        var xMovementCommand = _moveInput.ReadValue<Vector2>().x;
         if (xMovementCommand == 0)
             return;
 
         _character.UpdateFacing(xMovementCommand < 0 ? Direction.LEFT : Direction.RIGHT);
-    }
-
-    private void Interact()
-    {
-
     }
 }
--- Moving Through Mediums/Assets/Dev/Sylvie/InteractableBox.cs
+++ Moving Through Mediums/Assets/Dev/Sylvie/InteractableBox.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class InteractableBox : MonoBehaviour, IInteractable
 {
     private Rigidbody _rigidbody;
+    private Collider _collider;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
     }
 
     public void Interact(Character interactingCharacter)
@@ -14,6 +16,15 @@ public class InteractableBox : MonoBehaviour, IInteractable
         interactingCharacter.PickupBox(this);
     }
 
-    public void DisablePhysics() => _rigidbody.isKinematic = true;
-    public void EnablePhysics() => _rigidbody.isKinematic = false;
+    public void DisablePhysics()
+    {
+        _rigidbody.isKinematic = true;
+        _collider.enabled = false;
+    }
+
+    public void EnablePhysics()
+    {
+        _rigidbody.isKinematic = false;
+        _collider.enabled = true;
+    }
 }
