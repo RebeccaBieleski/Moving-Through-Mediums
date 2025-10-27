943e2aa5e01c07172c1b50fd90e0d2e5aa8efdb1
Sylky <miiyusli@gmail.com>
2025-10-28 11:36:58 +1300

    unsubscribe mouseclicks on scene change to fix picking up box

--- Moving Through Mediums/Assets/Scripts/MouseClicks.cs
+++ Moving Through Mediums/Assets/Scripts/MouseClicks.cs
@@ -8,13 +8,18 @@ public class MouseClicks : MonoBehaviour
     void Awake()
     {
         click = new InputAction(binding: "<Mouse>/leftButton");
-        click.performed += ctx => {
-            Vector3 coor = Mouse.current.position.ReadValue();
-            foreach (var hit in Physics.RaycastAll(Camera.main.ScreenPointToRay(coor)))
-            {
-                hit.collider.GetComponent<IClickable>()?.OnClick();
-            }
-        };
+        click.performed += OnMouseClick;
         click.Enable();
     }
+
+    private void OnMouseClick(InputAction.CallbackContext ctx)
+    {
+        Vector3 coor = Mouse.current.position.ReadValue();
+        foreach (var hit in Physics.RaycastAll(Camera.main.ScreenPointToRay(coor)))
+        {
+            hit.collider.GetComponent<IClickable>()?.OnClick();
+        }
+    }
+
+    private void OnDestroy() => click.performed -= OnMouseClick;
 }
