8c97be6fd5a9b92576198631766883a0575e2145
Sylky <miiyusli@gmail.com>
2025-10-27 19:10:33 +1300

    door makes ghost reappear when used

--- Moving Through Mediums/Assets/Scripts/EndDoor.cs
+++ Moving Through Mediums/Assets/Scripts/EndDoor.cs
@@ -1,17 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndDoor : MonoBehaviour
+public class EndDoor : MonoBehaviour, IClickable
 {
     [SerializeField] int NumberOfCharactersRequired = 3;
 
     private int _numberOfCharactersEntered;
 
+    private Character _currentCharacterAtDoor;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Character>() != null)
+        var player = other.GetComponentInChildren<GhostController>();
+        var character = other.GetComponent<Character>();
+        if (player != null && character != null)
         {
-            Destroy(other.gameObject);
+            _currentCharacterAtDoor = character;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponentInChildren<GhostController>();
+        var character = other.GetComponent<Character>();
+        if (player != null && character != null)
+        {
+            _currentCharacterAtDoor = null;
+        }
+    }
+
+    public void OnClick()
+    {
+        if (_currentCharacterAtDoor != null)
+        {
+            _currentCharacterAtDoor.GetComponentInChildren<GhostController>().Possess(null); // unpossess them
+            _currentCharacterAtDoor.transform.position = new Vector3(0, 0, 100);
+            _currentCharacterAtDoor = null;
             _numberOfCharactersEntered++;
 
             if (_numberOfCharactersEntered >= NumberOfCharactersRequired)
